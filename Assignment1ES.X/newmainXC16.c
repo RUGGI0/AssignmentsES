cae9585c4be2d171ce3fdb4f1232e459ff181a65
Gregorio Daneri <greg.daneri@gmail.com>
2026-03-03 15:59:01 +0100

    Refactor main function by commenting out unused code
    Commented out sections of code related to LED control and button handling.

--- Assignment1ES.X/newmainXC16.c
+++ Assignment1ES.X/newmainXC16.c
@@ -14,25 +14,33 @@ int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
     
     // Turn LD1 on + turn off
-    
+    /*
     TRISAbits.TRISA0 = 0;
     LATAbits.LATA0 = 1;
-    __delay_ms(1000);
+    __delay_ms(5000);
     LATAbits.LATA0 = 0;
-    
+    */
     
     // Turn LD1 on if button T2 is being pressed
-    /*
     TRISEbits.TRISE8 = 1;
     TRISAbits.TRISA0 = 0;
+    TRISEbits.TRISE9 = 1;
+    TRISGbits.TRISG9 = 0;
     while(1){
         if(PORTEbits.RE8 == 0)
             LATAbits.LATA0 = 1;
         else
             LATAbits.LATA0 = 0;
         __delay_us(500); // delay to avoid busy waiting
+        if(PORTEbits.RE9 == 0) // button clicked
+            while(1){
+                if(PORTEbits.RE9 == 1){
+                    // button released
+                    LATGbits.LATG9 = !LATGbits.LATG9; // toggle
+                    break;
+                } 
+            }
     }
-    */
     
     // Toggle LD1 for every click of T2 (LD1 starts turned off)
     /*
